0d2d77a8cae1a3fc9d2ddff167fb2148a15b37f6
veritakim <kjs3597@gmail.com>
2022-05-04 10:06:52 +0900

    login yup

--- freeboard_frontend/src/components/units/user/login/Login.container.tsx
+++ freeboard_frontend/src/components/units/user/login/Login.container.tsx
@@ -5,8 +5,29 @@ import { useRecoilState } from "recoil";
 import { accessTokenState, userInfomationState } from "../../../../commons/store";
 import LoginPresenter from "./Login.presenter";
 import { FETCH_USER_LOGGED_IN, LOGIN_USER } from "./Login.query";
+import * as yup from 'yup'
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+
+const schema = yup.object({
+  email: yup
+    .string()
+    .email("이메일 아이디를 @까지 정확하게 입력해주세요.")
+    .required("이메일은 필수 입력 사항입니다."),
+  password: yup
+    .string()
+    .matches(/^(?!(?:[0-9]+)$)([a-zA-Z]|[0-9a-zA-Z]){8,16}$/,
+                "영문+숫자를 조합 8~16자리의 비밀번호를 입력해주세요.")
+    .required("비밀번호는 필수 입력 사항입니다."),
+});
 
 export default function LoginContainer() {
+  const {register, handleSubmit, formState} = useForm({
+    resolver: yupResolver(schema),
+    mode: "onChange"
+  }
+  )
+
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [loginUser] = useMutation(LOGIN_USER)
@@ -54,7 +75,7 @@ export default function LoginContainer() {
       setUserInfo(userInfo)
       setAccessToken(accessToken);
 
-      localStorage.setItem("accessToken", accessToken)
+      // localStorage.setItem("accessToken", accessToken)
       // localStorage.setItem("userInfo", JSON.stringify(userInfo))
 
       router.push('/boards')
@@ -67,6 +88,8 @@ export default function LoginContainer() {
   
   return (
   <LoginPresenter
+    formState={formState}
+    register={register}
     onChangeEmail={onChangeEmail}
     onChangePassword={onChangePassword}
     onClickSubmit={onClickSubmit}
--- freeboard_frontend/src/components/units/user/login/Login.presenter.tsx
+++ freeboard_frontend/src/components/units/user/login/Login.presenter.tsx
@@ -1,12 +1,96 @@
+import styled from "@emotion/styled"
+
+const Wrapper = styled.div`
+  width: 580px;
+  height: 688px;
+  background-color: #F8F8F8;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+`
+
+const Inputdiv = styled.div`
+  width: 100%;
+  height: 306px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+  margin-bottom: 14.52px;
+  font-size: 30px;
+`
+
+const InputStyle = styled.input`
+  width: 480px;
+  height: 77.48px;
+  border: none;
+  border: 1px solid #cccccc;
+  background-color: #ffffff;
+  font-size: 18px;
+  color: #6B6B6B;
+  padding-left: 20px;
+  margin-top: 10px;
+`
+const Error = styled.div`
+  color: #EF3030;
+  font-size: 16px;
+  margin-bottom: 35px;
+`
+
+const LoginTitle = styled.div`
+  width: 100%;
+  height: 120px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+  font-size: 30px;
+  font-weight: 700;
+`
+
+const BtnArea = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+`
+
+const LoginBtn = styled.div`
+  width: 120px;
+  height: 45px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #918d87;
+  color: #ffffff;
+  font-size: 20px;
+  font-weight: 600;
+  cursor: pointer;
+`
+
 
 export default function LoginPresenter (props: any) {
 
   return (
-    <div>
-      <h1>로그인</h1>
-      <div>이메일: <input type="text" onChange={props.onChangeEmail} /></div>
-      <div>비밀번호: <input type="password" onChange={props.onChangePassword} /></div>
-      <button onClick={props.onClickSubmit}>로그인 하기</button>
-    </div>
+    <Wrapper>
+      <form onClick={props.onClickSubmit}>
+      <LoginTitle>로그인</LoginTitle>
+          <Inputdiv>
+            <InputStyle type="text" onKeyUp={props.onChangeEmail} placeholder="이메일" {...props.register("email")} />
+            <Error>{props.formState.errors.email?.message}</Error>
+          {/* </Inputdiv>
+          
+          <Inputdiv> */}
+            <InputStyle type="password" onKeyUp={props.onChangePassword} placeholder="비밀번호" {...props.register("password")} />
+            <Error>{props.formState.errors.password?.message}</Error>
+          </Inputdiv>
+
+          <BtnArea>
+            <LoginBtn>로그인 하기</LoginBtn>
+          </BtnArea>
+      </form>
+    </Wrapper>
   )
 }
