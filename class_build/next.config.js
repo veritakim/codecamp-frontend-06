215efc34fff12d622d61cbd79621c0db1c91e170
veritakim <kjs3597@gmail.com>
2022-05-05 13:25:35 +0900

    bbb

--- class_build/next.config.js
+++ class_build/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true
+  trailingSlash: true,
+  generateBuildId: () => "trudru",
+  exportPathMap: () => ({
+    "/": { page: "/"},
+    "/board": {page: "/board"},
+    "/404": {page: "/404"}
+  })
 }
 
 module.exports = nextConfig
